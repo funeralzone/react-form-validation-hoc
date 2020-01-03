8dddeef8294486adc3c7b0055921b2aefd8a3f0e
Nick Hyde <nickhyde@Nicks-MacBook-Pro.local>
2020-01-03 14:05:10 +0000

    Fix for scrollTo position inside a parent element

--- utils.js
+++ utils.js
@@ -1,28 +1,23 @@
 export const scrollToElement = (elementRef, scrollableParentRef) => {
   const elementCheck = setInterval(() => {
-    const element = elementRef.current;
+    const element = elementRef && elementRef.current;
+    const scrollingElement = scrollableParentRef && scrollableParentRef.current;
 
-    if (scrollableParentRef) {
-      const scrollingElement = scrollableParentRef && scrollableParentRef.current;
-      if (elementRef.current && scrollingElement) {
-        scrollingElement.scrollTo({
-          top: element.getBoundingClientRect().top - scrollingElement.offsetTop,
-          left: 0,
-          behavior: 'smooth',
-        });
-      }
-    } else {
+    if (scrollableParentRef && element) {
+      scrollingElement.scrollTo({
+        top: element.offsetTop - scrollingElement.offsetTop,
+        left: 0,
+        behavior: 'smooth',
+      });
+    } else if (element) {
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      if (elementRef.current) {
-        window.scrollTo({
-          top: element.getBoundingClientRect().top + scrollTop,
-          left: 0,
-          behavior: 'smooth',
-        });
-      }
+      window.scrollTo({
+        top: element.getBoundingClientRect().top + scrollTop,
+        left: 0,
+        behavior: 'smooth',
+      });
     }
 
     clearInterval(elementCheck);
   }, 100);
 };
-
